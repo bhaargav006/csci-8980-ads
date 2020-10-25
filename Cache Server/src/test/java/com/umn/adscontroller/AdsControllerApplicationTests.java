87893ed1ef0783974d460eb1de5abbed66c54cc3
Sid Suresh <suresh.siddharth@gmail.com>
2020-10-25 17:02:57 -0500

    Initial Commit

---
+++ Cache Server/src/test/java/com/umn/adscontroller/AdsControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umn.adscontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Controller/src/main/java/com/umn/adscontroller/AdsControllerApplication.java
@@ -0,0 +1,13 @@
+package com.umn.adscontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdsControllerApplication.class, args);
+    }
+
+}
