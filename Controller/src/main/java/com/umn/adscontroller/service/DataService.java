43b5bd8b0b2798ccc9b94d400a3cc7c0edc5ce04
Sid Suresh <suresh.siddharth@gmail.com>
2020-11-01 22:14:21 -0600

    Adds hashing to controller

--- Controller/src/main/java/com/umn/adscontroller/service/DataService.java
+++ Controller/src/main/java/com/umn/adscontroller/service/DataService.java
@@ -2,6 +2,7 @@ package com.umn.adscontroller.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,15 +15,38 @@ public class DataService {
         this.restTemplate = restTemplate;
     }
 
-    public String getData(String inputKey) {
-        // TODO: Implement hashing function to get URL
+    @Value("${controller.server.list}")
+    private String[] arrayOfStrings;
 
-        return restTemplate.getForObject("http://localhost:8081/api/" + inputKey, String.class);
+
+    public String getData(String inputKey) {
+        int serverId = computeHash(inputKey);
+
+        String baseUrl = arrayOfStrings[serverId];
+        String url = String.format("http://%s/api/%s", baseUrl, inputKey);
+
+        return restTemplate.getForObject(url, String.class);
     }
 
     public void putData(String inputKey, String inputValue) {
-        // TODO: Implement hashing function to get URL
+        int serverId = computeHash(inputKey);
 
-        restTemplate.put("http://localhost:8081/api/" + inputKey, inputValue);
+        String baseUrl = arrayOfStrings[serverId];
+        String url = String.format("http://%s/api/%s", baseUrl, inputKey);
+
+        restTemplate.put(url, inputValue);
+    }
+
+    private int computeHash(String inputKey) {
+        int p = 53;
+        double m = 1e9 + 9;
+        double hashValue = 0;
+        double pPow = 1;
+
+        for (char c : inputKey.toCharArray()) {
+            hashValue = (hashValue + (c - 'a' + 1) * pPow) % m;
+            pPow = (pPow * p) % m;
+        }
+        return (int) hashValue % 3;
     }
 }
